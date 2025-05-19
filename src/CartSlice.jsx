ed0d44f009e3e1035fa5b33fd1b1858c638f5f0c
Sneha Sinha <2305495@kiit.ac.in>
2025-05-19 16:59:48 +0530

    Updated CartSlice.jsx and ProductSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,7 +7,17 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const existingIndex = state.items.findIndex(
+        (item) => item.name === action.payload.name
+      );
+
+      if (existingIndex >= 0) {
+        // If exists, increase quantity (if you want to track quantity)
+        state.items[existingIndex].quantity += 1;
+      } else {
+        // If not exists, add item with quantity = 1
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
     },
     removeItem: (state, action) => {
     },
